c800b29ec4e3123c80b5ca035e7aef6ae46e42db
Gabriel Lucas <53130753+UmloucoBR@users.noreply.github.com>
2022-07-27 23:54:51 -0300

    The basics.

--- provincia.cpp
+++ provincia.cpp
@@ -1,1 +1,18 @@
-﻿
+﻿#include <iostream>
+#include <string>
+
+void run(std::string string) {
+	std::cout << string;
+}
+
+int main(int argc, char* argv[]) {
+	if (argc == 2)
+	{
+		run(argv[0]);
+	}
+	else
+	{
+		std::cout << "How to use the program: provincia <archive_name>.\n";
+		return -1;
+	}
+}
